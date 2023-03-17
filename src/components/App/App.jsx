24b71e99963540c3a60b0df00f9594bed80d1462
Dina-Ursula <dinatyan26@gmail.com>
2023-03-18 01:15:45 +0200

    fix-bug

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import Searchbar from '../Searchbar/Searchbar';
 import Modal from '../Modal/Modal';
